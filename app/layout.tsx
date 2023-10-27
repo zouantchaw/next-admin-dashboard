582e829b2f283ff575ee7349fd7186430a968ab7
Wielfried Zouantcha <zouantchaw74@gmail.com>
2023-10-27 16:00:44 -0400

    Add dashboard layout component and update root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/* <body>{children}</body>
-       */}
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
