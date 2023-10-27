758fc0606d24d29047a9852baaefaf24521de10b
Wielfried Zouantcha <zouantchaw74@gmail.com>
2023-10-27 15:24:21 -0400

    Add global CSS and border styles to layout and page components

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,9 @@ export default function Page() {
               Next.js Learn Course
             </a>
             , brought to you by Vercel.
+            <div
+  className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
+/>
           </p>
           <Link
             href="/login"
